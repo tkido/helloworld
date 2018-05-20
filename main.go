56c0e372cde16a4ff14bbd6732d3bbf194419bf2
tkido <takanorikido@gmail.com>
2018-05-20 09:24:49 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"image"
 	"image/color"
 	_ "image/png"
 	"log"
@@ -9,24 +8,16 @@ import (
 
 	"math/rand"
 
-	"bitbucket.org/tkido/helloworld/quadtree"
-
-	"bitbucket.org/tkido/helloworld/vector"
 	"github.com/hajimehoshi/ebiten"
 )
 
 const (
-	screenWidth  = 512
-	screenHeight = 512
+	screenWidth  = 640
+	screenHeight = 480
 )
 
 var (
-	imgSrc   *ebiten.Image
-	game     Game
-	manager  *quadtree.Manager
-	balls    []*Ball
-	count    int
-	srcRects []*image.Rectangle
+	game Game
 )
 
 // Game is status of game
@@ -36,63 +27,15 @@ type Game struct {
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-
-	f, err := Assets.Open("/assets/nc35542small.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	png, _, err := image.Decode(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	imgSrc, err = ebiten.NewImageFromImage(png, ebiten.FilterDefault)
-	if err != nil {
-		log.Fatal(err)
-	}
-	w, h := imgSrc.Size()
-	rect0 := image.Rect(0, 0, w/2, h)
-	rect1 := image.Rect(w/2, 0, w, h)
-	srcRects = []*image.Rectangle{&rect0, &rect1}
-
 	game = Game{true, false}
-	manager = quadtree.NewManager(screenWidth, screenHeight, 3)
 }
 
 func update(screen *ebiten.Image) (err error) {
-	if balls == nil {
-		balls = []*Ball{}
-		for i := 0; i < 200; i++ {
-			r := rand.Float64()*10 + 5
-			b := NewBall(
-				r,
-				vector.Vector{X: rand.Float64()*(screenWidth-2*r) + r, Y: rand.Float64()*(screenHeight-2*r) + r},
-				vector.Vector{X: (rand.Float64() - 0.5) * 4, Y: (rand.Float64() - 0.5) * 4},
-				imgSrc,
-			)
-
-			balls = append(balls, b)
-			manager.Update(b, b.P.X-b.R, b.P.Y-b.R, b.P.X+b.R, b.P.Y+b.R)
-		}
-		return
-	}
-	for _, b := range balls {
-		b.Update()
-		manager.Update(b, b.P.X-b.R, b.P.Y-b.R, b.P.X+b.R, b.P.Y+b.R)
-	}
-	count = 0
-	manager.Check()
-	// fmt.Println(count)
-
 	return
 }
 
 func draw(screen *ebiten.Image) (err error) {
 	screen.Fill(color.NRGBA{0x00, 0xff, 0x00, 0xff})
-
-	for _, ball := range balls {
-		ball.Draw(screen)
-	}
 
 	if game.IsDebugPrint {
 		err = debugPrint(screen)
@@ -135,7 +78,6 @@ func control() (err error) {
 	} else if ebiten.IsKeyPressed(ebiten.KeyF4) {
 		game.IsDebugPrint = !game.IsDebugPrint
 	} else if ebiten.IsKeyPressed(ebiten.KeyR) {
-		balls = nil
 	}
 
 	return
