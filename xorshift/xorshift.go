9d0481b58e8a13b3ad11dd63ecf800bb74b1cdbb
Takanori Kido <tkido@xevo.com>
2018-05-23 11:15:55 +0900

    update

--- xorshift/xorshift.go
+++ xorshift/xorshift.go
@@ -1,1 +1,30 @@
 package xorshift
+
+import (
+	"math"
+)
+
+// default seed
+var x uint64 = 88172645463325252
+var max = float64(math.MaxUint64)
+
+// Seed gave seed
+func Seed(seed int64) {
+	x = uint64(seed)
+}
+
+func next() uint64 {
+	x = x ^ (x << 7)
+	x = x ^ (x >> 9)
+	return x
+}
+
+// Float64 returns, as a float64, a pseudo-random number in [0.0,1.0) from the default Source.
+func Float64() float64 {
+	return float64(next()) / max
+}
+
+// Int returns a non-negative pseudo-random int from the default Source.
+func Int() int {
+	return int(next())
+}
--- xorshift/xorshift_test.go
+++ xorshift/xorshift_test.go
@@ -1,20 +1,54 @@
 package xorshift
 
 import (
+	"crypto/md5"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"testing"
+	"time"
 )
 
+const intSize = 32 << (^uint(0) >> 63)
+
 func TestXor(t *testing.T) {
+	Seed(time.Now().UnixNano())
 	for i := 0; i < 10; i++ {
-		fmt.Println(xor())
+		fmt.Println(Float64())
+	}
+	for i := 0; i < 10; i++ {
+		fmt.Println(Int())
 	}
 }
 
-var x uint64 = 88172645463325252
+func TestSeed(t *testing.T) {
+	i := time.Now().UnixNano()
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(i))
+	fmt.Printf("%v\n", i)
+	fmt.Printf("%x\n", i)
+	fmt.Printf("%x\n", b)
+}
+func TestMd5(t *testing.T) {
+	fmt.Println(intSize)
+	h := md5.New()
 
-func xor() uint64 {
-	x = x ^ (x << 7)
-	x = x ^ (x >> 9)
-	return x
+	io.WriteString(h, "testa")
+	sum := h.Sum(nil)
+	fmt.Printf("%x\n", sum)
+	fmt.Println(len(sum))
+
+	var u0, u1 uint64
+	var i uint
+	for i = 0; i < 8; i++ {
+		fmt.Printf("%x\n", u0)
+		fmt.Printf("%x\n", sum[i])
+		u0 = u0<<8 | uint64(sum[i])
+	}
+	for i = 0; i < 8; i++ {
+		u1 = u1<<8 | uint64(sum[i+8])
+	}
+	fmt.Printf("%x\n", u0)
+	fmt.Printf("%x\n", u1)
+
 }
