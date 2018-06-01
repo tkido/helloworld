b895568e353896f4d2d6baedc0f1460071842d9a
Takanori Kido <tkido@xevo.com>
2018-06-01 20:22:45 +0900

    update

---
+++ ui/callback.go
@@ -0,0 +1,16 @@
+package ui
+
+// Callback is callback function on event
+type Callback func(item Item)
+
+// Callbacks keep callback functions
+type Callbacks map[EventType]Callback
+
+// SetCallback set callback function to item. When set nil, it means delete
+func (cs Callbacks) SetCallback(t EventType, c Callback) {
+	if c == nil {
+		delete(cs, t)
+	} else {
+		cs[t] = c
+	}
+}
