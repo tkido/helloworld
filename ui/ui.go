5a7728d486cdf63e433ded8bcf84acd3ac42163a
Takanori Kido <tkido@xevo.com>
2018-06-08 13:21:41 +0900

    update

--- ui/ui.go
+++ ui/ui.go
@@ -14,14 +14,12 @@ func Update(bg Item) {
 		click := LeftClick + EventType(i)
 		if m.Clicked[i] != nil {
 			if m.Now-m.Clicked[i].Frame > doubleClickInterval {
-				if c, ok := m.Clicked[i].Item.Callbacks[click]; ok {
-					c(m.Clicked[i].Item.Sub)
-				}
+				m.Clicked[i].Item.Call(click)
 				m.Clicked[i] = nil
 			}
 		}
 	}
-
+	// mouse control
 	if ev, ok := m.getMouseEvent(); ok {
 		bg.HandleMouseEvent(ev, image.ZP, bg.Rectangle())
 		for k, v := range m.InItems {
