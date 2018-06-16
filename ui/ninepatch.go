35a11cc7031ee3e9e40f25e35a285dd237ffedf7
tkido <takanorikido@gmail.com>
2018-06-16 13:20:01 +0900

    update

--- ui/ninepatch.go
+++ ui/ninepatch.go
@@ -23,29 +23,15 @@ func NewNinepatch(w, h int, srcImg image.Image) *Ninepatch {
 }
 
 func makeNineRects(out, in image.Rectangle) []image.Rectangle {
-	s := []image.Rectangle{}
-	for k := 0; k < 9; k++ {
-		var x0, y0, x1, y1 int
-		i, j := k%3, k/3
-		switch i {
-		case 0:
-			x0, x1 = out.Min.X, in.Min.X
-		case 1:
-			x0, x1 = in.Min.X, in.Max.X
-		case 2:
-			x0, x1 = in.Max.X, out.Max.X
+	rs := make([]image.Rectangle, 0, 9)
+	ys := [4]int{out.Min.Y, in.Min.Y, in.Max.Y, out.Max.Y}
+	xs := [4]int{out.Min.X, in.Min.X, in.Max.X, out.Max.X}
+	for y := 0; y < 3; y++ {
+		for x := 0; x < 3; x++ {
+			rs = append(rs, image.Rect(xs[x], ys[y], xs[x+1], ys[y+1]))
 		}
-		switch j {
-		case 0:
-			y0, y1 = out.Min.Y, in.Min.Y
-		case 1:
-			y0, y1 = in.Min.Y, in.Max.Y
-		case 2:
-			y0, y1 = in.Max.Y, out.Max.Y
-		}
-		s = append(s, image.Rect(x0, y0, x1, y1))
 	}
-	return s
+	return rs
 }
 
 // Reflesh updates internal *ebiten.Image
@@ -59,11 +45,11 @@ func (n *Ninepatch) Reflesh() {
 	tiSize := image.Point{to.Dx() - s[0].Dx() - s[2].Dx(), to.Dy() - s[0].Dy() - s[6].Dy()}
 	ti := image.Rectangle{si.Min, si.Min.Add(tiSize)}
 	t := makeNineRects(to, ti)
-	for k := 0; k < 9; k++ {
+	for i := 0; i < 9; i++ {
 		op := &ebiten.DrawImageOptions{}
-		op.SourceRect = &(s[k])
-		op.GeoM.Scale(float64(t[k].Dx())/float64(s[k].Dx()), float64(t[k].Dy())/float64(s[k].Dy()))
-		op.GeoM.Translate(float64(t[k].Min.X), float64(t[k].Min.Y))
+		op.SourceRect = &(s[i])
+		op.GeoM.Scale(float64(t[i].Dx())/float64(s[i].Dx()), float64(t[i].Dy())/float64(s[i].Dy()))
+		op.GeoM.Translate(float64(t[i].Min.X), float64(t[i].Min.Y))
 		n.Image.DrawImage(src, op)
 	}
 }
