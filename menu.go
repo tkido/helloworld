a893bc6786dd30c72d6b9b18ad80ee8e99f7a87f
tkido <takanorikido@gmail.com>
2018-06-16 13:42:01 +0900

    Update

--- menu.go
+++ menu.go
@@ -24,7 +24,6 @@ func menuScreen() *ui.Box {
 
 	data := []string{"バナナ", "りんご", "オレンジ", "とうもろこし", "焼き肉", "北京ダック", "豚の丸焼き"}
 	bg = ui.NewBox(screenWidth, screenHeight, color.NRGBA{0x00, 0xff, 0x00, 0xff})
-	// menu := ui.NewBox(400, 400, color.NRGBA{0x00, 0xff, 0xff, 0xff})
 	menu := ui.NewNinepatch(400, 400, png)
 	bg.Add(10, 10, menu)
 
@@ -38,23 +37,18 @@ func menuScreen() *ui.Box {
 
 	for i, s := range data {
 		label := ui.NewLabel(400, 30, s, 0, color.NRGBA{0x00, 0xff, 0x00, 0xff}, nil)
-		onSelect := func(i int) func(item ui.Item) {
+		label.SetCallback(ui.LeftClick, func(i int) func(item ui.Item) {
 			return func(item ui.Item) {
 				result.SetText(data[i])
 			}
-		}(i)
-		label.SetCallback(ui.LeftClick, onSelect)
+		}(i))
 		label.SetCallback(ui.MouseOver, func(item ui.Item) {
 			op := &ebiten.DrawImageOptions{}
 			op.GeoM.Translate(20, 0)
 			item.SetDIO(op)
-			// x, y := item.Position()
-			// item.Move(x+20, y)
 		})
 		label.SetCallback(ui.MouseOut, func(item ui.Item) {
 			item.SetDIO(nil)
-			// x, y := item.Position()
-			// item.Move(x-20, y)
 		})
 		menu.Add(8, 8+i*30, label)
 	}
