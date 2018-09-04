bd405d1480ae32b855ba45d89e9fe7fb486f6308
tkido <takanorikido@gmail.com>
2018-09-04 20:33:02 +0900

    update

--- core/godfather/godfather.go
+++ core/godfather/godfather.go
@@ -5,12 +5,25 @@ import (
 	"time"
 )
 
+// for data.go
+const (
+	MaleSingle = iota
+	MalePrefix
+	MaleSuffix
+	FemaleSingle
+	FemalePrefix
+	FemaleSuffix
+)
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 100; i++ {
+		rand.Int()
+	}
 }
 
 // Next take next name
-func Next() string {
+func Next(gender int) string {
 	max := len(data[0])
 	return data[0][rand.Intn(max)]
 }
--- core/godfather/godfather_test.go
+++ core/godfather/godfather_test.go
@@ -4,14 +4,48 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
-	"time"
 )
 
-func TestNew(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
-	fmt.Println(len(data))
-	max := len(data[0])
-	for i := 0; i < 10; i++ {
-		fmt.Println(data[0][rand.Intn(max)])
+func TestNewMaleSingle(t *testing.T) {
+	fmt.Println("男性単体名")
+	sin := data[MaleSingle]
+	max := len(sin)
+	for i := 0; i < 40; i++ {
+		s := sin[rand.Intn(max)]
+		fmt.Println(s)
+	}
+}
+
+func TestNewMaleCompound(t *testing.T) {
+	fmt.Println("男性合成名")
+	pre := data[MalePrefix]
+	suf := data[MaleSuffix]
+	maxPre := len(pre)
+	maxSuf := len(suf)
+	for i := 0; i < 40; i++ {
+		s := pre[rand.Intn(maxPre)] + suf[rand.Intn(maxSuf)]
+		fmt.Println(s)
+	}
+}
+
+func TestNewFemaleSingle(t *testing.T) {
+	fmt.Println("女性単体名")
+	sin := data[FemaleSingle]
+	max := len(sin)
+	for i := 0; i < 40; i++ {
+		s := sin[rand.Intn(max)]
+		fmt.Println(s)
+	}
+}
+
+func TestNewFemaleCompound(t *testing.T) {
+	fmt.Println("女性合成名")
+	pre := data[FemalePrefix]
+	suf := data[FemaleSuffix]
+	maxPre := len(pre)
+	maxSuf := len(suf)
+	for i := 0; i < 40; i++ {
+		s := pre[rand.Intn(maxPre)] + suf[rand.Intn(maxSuf)]
+		fmt.Println(s)
 	}
 }
